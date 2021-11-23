7d61c822ba4cb21999a245df6b6013d433c84627
Greydne Scott <greydenscott@gmail.com>
2021-11-23 19:11:00 +1000

    completed game tests

--- src/services/__tests__/game.test.ts
+++ src/services/__tests__/game.test.ts
@@ -1,4 +1,5 @@
 /* eslint-disable camelcase */
+import { WinnerData, GameData } from '../../@types/game'
 import { CryptoListing, CryptoPlayer } from '../../@types/crypto'
 import * as GameServce from '../game'
 
@@ -112,5 +113,28 @@ describe('cryptoListingsToCryptoPlayers', () => {
     const cryptoList: CryptoListing[] = listings
     const cryptoPlayers: CryptoPlayer[] = GameServce.cryptoListingsToCryptoPlayers(cryptoList)
     expect(cryptoPlayers).not.toEqual(cryptoList)
+  })
+})
+
+describe('pickWinner', () => {
+  it('should pick a winner and return that winners place in the list', () => {
+    const cryptoList: CryptoListing[] = listings
+    const winnerData: WinnerData = GameServce.pickWinner(cryptoList)
+    expect(winnerData.winnerPosition).toEqual(cryptoList.findIndex((player) => player === winnerData.winner))
+  })
+})
+
+describe('constructGame', () => {
+  it('should return a game of the correct size', async () => {
+    const gameSize: number = 2
+    const cryptoList: CryptoListing[] = listings
+    const game: GameData = await GameServce.constructGame(cryptoList, gameSize)
+    expect(game.specifications.gameSize).toEqual(gameSize)
+  })
+  it('should have the correct winnerPosition position in the participants array', async () => {
+    const gameSize: number = 2
+    const cryptoList: CryptoListing[] = listings
+    const game: GameData = await GameServce.constructGame(cryptoList, gameSize)
+    expect(game.specifications.winnerPosition).toEqual(game.participants.findIndex((player) => player.id === game.winner.id))
   })
 })
